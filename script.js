c7d914f69aab2b3c7b04704622e0c231267fcfac
Delial01 <logigan_delia@yahoo.com>
2023-04-27 14:23:50 +0100

    Correct error in script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let bot = new RiveScript();
 
-const brains = ["brain/subs.rive", "brain/delia.rive", "brain/delia.rive"];
+const brains = ["brain/subs.rive", "brain/delia.rive", "brain/ruben.rive"];
 
 bot.loadFile(brains).then(botReady).catch(botNotReady);
 
